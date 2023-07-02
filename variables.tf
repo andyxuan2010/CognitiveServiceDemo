2a0e12199c03f5f23a3a2ff5bac0994b06fc58a4
andyxuan2010 <andyxuan2010@gmail.com>
2023-07-02 10:28:40 -0400

    add cognitive service

--- variables.tf
+++ variables.tf
@@ -79,3 +79,12 @@ variable "network_interface_name" {
   default     = "nic-vm-1"
 }
 
+
+
+# for WestEurope location we have "B1, B2, S0, S1, S2, S4, D1"
+# for demo purpose we pick D1 plan
+variable "sku" {
+  type        = string
+  description = "The sku name of the Azure Analysis Services server to create. Choose from: B1, B2, D1, S0, S1, S2, S3, S4, S8, S9. Some skus are region specific. See https://docs.microsoft.com/en-us/azure/analysis-services/analysis-services-overview#availability-by-region"
+  default     = "D1"
+}
