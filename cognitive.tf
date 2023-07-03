9e157cdc2e5bf5f9137e89bd87fa7f9339eb2106
andyxuan2010 <andyxuan2010@gmail.com>
2023-07-03 08:37:54 -0400

    add private endpiont for cognitive service

--- cognitive.tf
+++ cognitive.tf
@@ -18,16 +18,17 @@ resource "azurerm_cognitive_account" "cognitive-service" {
 }
 
 
-# Create a private endpoint for Azure Cognitive Services
-# resource "azurerm_private_endpoint" "edp-cognitive" {
-#   name                = "edp-cognitive-service"
-#   location            = azurerm_resource_group.challenge-rg.location
-#   resource_group_name = azurerm_resource_group.challenge-rg.name
+#Create a private endpoint for Azure Cognitive Services
+resource "azurerm_private_endpoint" "edp-cognitive" {
+  name                = "edp-cognitive-service"
+  location            = azurerm_resource_group.challenge-rg.location
+  resource_group_name = azurerm_resource_group.challenge-rg.name
+  subnet_id           = azurerm_subnet.challenge-subnet.id
 
-#   subnet_id = azurerm_subnet.challenge-subnet.id
-#   private_service_connection {
-#     name                           = "example-service-connection"
-#     private_connection_resource_id = azurerm_cognitive_account.cognitive-service.id
-#     subresource_names              = ["ml"]
-#   }
-# }
+  private_service_connection {
+    name                           = "cognitive-private-connection"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_cognitive_account.cognitive-service.id
+    subresource_names              = ["ml"]
+  }
+}
